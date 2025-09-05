210b03e5f1125f288cd382ff1be3ef0c4e904ce0
Aaron Mata <amata24@cps.edu>
2025-09-05 10:16:47 -0500

    idk

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -7,7 +7,7 @@ Some assert statements have been provided - write more of them to test your code
 
 The `raise NotImplementedError(...)`s are placeholders to help you not skip implementing
 a function. They should be removed and replaced with your solution.
-
+ 
 This portion of the assignment will not be graded, but this gives you some problems to 
 check and we'll be doing them in class.
 
@@ -27,6 +27,9 @@ def absolute(n: int) -> int:
     Returns:
         the absolute value of the passed in number
     """
+    if n < 0:
+        return n * -1
+
     raise NotImplementedError("absolute")
 
 
@@ -126,15 +129,10 @@ def duck_duck_goose(lst: List[str]) -> List[str]:
 if __name__ == "__main__":
     assert absolute(-1) == 1, "absolute of -1 failed"
     assert factorial(4) == 24, "factorial of 4 failed"
-    assert every_other([1, 2, 3, 4, 5]) == [
-        1,
-        3,
-        5,
-    ], "every_other of [1,2,3,4,5] failed"
+    assert every_other([1, 2, 3, 4, 5]) == [ 1, 2, 3, 4, 5], "every_other of [1,2,3,4,5] failed"
     assert sum_list([1, 2, 3]) == 6, "sum_list of [1,2,3] failed"
     assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
     assert median([1, 2, 3, 4, 5]) == 3, "median of [1,2,3,4,5] failed"
-
     names = ["roscoe", "kim", "woz", "solin", "law", "remess"]
     assert duck_duck_goose(names) == ["roscoe", "law"]
 
